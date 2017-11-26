29034258f910d83bd1a33aa21acc83e90f8b8777
kienha <vukienha@gmail.com>
2017-11-26 16:31:13 +0700

    first commit

---
+++ demoquanlysv/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(demoquanlysv.Startup))]
+namespace demoquanlysv
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
